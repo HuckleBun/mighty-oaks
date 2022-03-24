41eadda6a81de07e9f9e0842d4e3105b82680c09
unknown <micah@theduehrings.com>
2022-03-24 14:18:40 -0500

    setup server.js file

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 app.use('/api', apiRoutes);
